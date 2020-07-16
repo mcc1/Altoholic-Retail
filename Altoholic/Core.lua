909ccd2f1b4011153f2f8da6825327ab4e5aa67a
teelolws <49792789+teelolws@users.noreply.github.com>
2020-07-16 13:20:46 +1200

    Version update: 018

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -4,8 +4,8 @@ _G[addonName] = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "A
 
 local addon = _G[addonName]
 
-addon.Version = "v8.3.017"
-addon.VersionNum = 803017
+addon.Version = "v8.3.018"
+addon.VersionNum = 803018
 
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
 local commPrefix = addonName
