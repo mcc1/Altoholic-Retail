c6d34ef6865b31566bf664647369ee713c89fa58
teelolws <49792789+teelolws@users.noreply.github.com>
2020-07-26 09:52:58 +1200

    Revert "Revert "024""
    This reverts commit 5760f8a0e7a63e9f86f8f8d952dd3052f519923e.

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -4,8 +4,8 @@ _G[addonName] = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "A
 
 local addon = _G[addonName]
 
-addon.Version = "v8.3.023"
-addon.VersionNum = 803023
+addon.Version = "v8.3.024"
+addon.VersionNum = 803024
 
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
 local commPrefix = addonName
