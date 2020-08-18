07a5d7300b315d47f37a6ce45fda56bd3c7c6750
teelolws <49792789+teelolws@users.noreply.github.com>
2020-08-18 14:51:29 +1200

    bugfix - compare string with number

--- Altoholic/libs/LibCraftCategories-1.0/LibCraftCategories-1.0.lua
+++ Altoholic/libs/LibCraftCategories-1.0/LibCraftCategories-1.0.lua
@@ -9,7 +9,7 @@ Converted to Lua using: https://thdoan.github.io/mr-data-converter/
 
 --]]
 
-local LIB_VERSION_MAJOR, LIB_VERSION_MINOR = "LibCraftCategories-1.0", 2
+local LIB_VERSION_MAJOR, LIB_VERSION_MINOR = "LibCraftCategories-1.0", 1
 local lib = LibStub:NewLibrary(LIB_VERSION_MAJOR, LIB_VERSION_MINOR)
 
 if not lib then return end -- No upgrade needed
@@ -18,11 +18,11 @@ local GAME_LOCALE = GetLocale()
 
 local function initialize()
     table.sort(lib.enUS, function(k1, k2)
-            return k1.OrderIndex < k2.OrderIndex
+            return tonumber(k1.OrderIndex) < tonumber(k2.OrderIndex)
         end)
     if GAME_LOCALE ~= "enUS" then
         table.sort(lib[GAME_LOCALE], function(k1, k2)
-                return k1.OrderIndex < k2.OrderIndex
+                return tonumber(k1.OrderIndex) < tonumber(k2.OrderIndex)
             end)
     end
 end
