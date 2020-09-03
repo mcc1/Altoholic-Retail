486fee04d4b26d5b3fb3f75e31bbf00a9829fdaa
teelolws <49792789+teelolws@users.noreply.github.com>
2020-09-03 19:06:51 +1200

    Grid realms to account change

--- Altoholic_Grids/Grid_WarCampaignFollowers.lua
+++ Altoholic_Grids/Grid_WarCampaignFollowers.lua
@@ -22,24 +22,26 @@ local function BuildView()
 	-- list all collected followers (across all alts), sorted alphabetically
 	-- .. then list all uncollected followers, also sorted alphabetically
 
-	local account, realm = AltoholicTabGrids:GetRealm()
+	local account = AltoholicTabGrids:GetAccount()
 	local uncollected = {}
 	local followers
 	
-	-- Prepare a list of all collected followers across all alts on this realm
-	for characterKey, character in pairs(DataStore:GetCharacters(realm, account)) do
-		followers = DataStore:GetBFAFollowers(character)
-		
-		if followers then
-			for id, _ in pairs(followers) do
-				-- temporary fix: follower keys have been replaced from their name (string) to their id (numeric)
-				-- fix it here instead of in datastore, which is already ok.
-				if (type(id) == "number") then
-					collected[id] = true	-- [123] = true
-				end
-			end
-		end
-	end
+	-- Prepare a list of all collected followers across all alts on this account
+    for realm in pairs(DataStore:GetRealms(account)) do
+    	for characterKey, character in pairs(DataStore:GetCharacters(realm, account)) do
+    		followers = DataStore:GetBFAFollowers(character)
+    		
+    		if followers then
+    			for id, _ in pairs(followers) do
+    				-- temporary fix: follower keys have been replaced from their name (string) to their id (numeric)
+    				-- fix it here instead of in datastore, which is already ok.
+    				if (type(id) == "number") then
+    					collected[id] = true	-- [123] = true
+    				end
+    			end
+    		end
+    	end
+    end
 	
 	-- Prepare a list of uncollected followers
 	local followersList = C_Garrison.GetFollowers(Enum.GarrisonFollowerType.FollowerType_8_0)
