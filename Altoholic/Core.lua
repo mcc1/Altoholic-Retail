70282a69d393fde42c9cd99d515ee1c7d796073c
teelolws <49792789+teelolws@users.noreply.github.com>
2020-10-11 20:32:31 +1300

    %project version% change

--- Altoholic/Core.lua
+++ Altoholic/Core.lua
@@ -4,10 +4,7 @@ _G[addonName] = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "A
 
 local addon = _G[addonName]
 
-addon.VersionNum = GetAddOnMetadata("Altoholic", "Version")
-local pattern = ".*(%d+).*(%d+).*(%d).*(%d%d%d).*"
-local expansionID, patchID, subpatchID, versionID = addon.VersionNum:match(pattern)
-addon.Version = "v"..expansionID.."."..patchID.."."..subpatchID.."."..versionID
+addon.Version = GetAddOnMetadata("Altoholic", "Version")
 
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
 local commPrefix = addonName
